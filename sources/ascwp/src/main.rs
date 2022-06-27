6fd5aba4aab4f385de86e61faa17084f27053b66
parthx <parth3sh@gmail.com>
2022-06-27 16:26:10 +0000

    Added autoscaling.should-wait settings to k8s variants.

--- sources/ascwp/src/main.rs
+++ sources/ascwp/src/main.rs
@@ -1,18 +1,35 @@
-use tokio::runtime::Runtime;
 use imdsclient::ImdsClient;
+use imdsclient::Error;
 use snafu::{OptionExt, ResultExt};
+use tokio::runtime::Runtime;
+use std::env;
 
-async fn get_lifecycle_state(client: &mut ImdsClient) -> String {
-    client
-        .fetch_lifecycle_state().await
-            .unwrap()
-            .unwrap()
+//Uses function from imds client to fetch lifecycle state
+async fn get_lifecycle_state(client: &mut ImdsClient) -> Result<Option<String>, Error> {
+    client.fetch_lifecycle_state().await
 }
-fn main(){
+
+//Unwraps result and option
+fn unwrap_state(state: Result<Option<String>, Error>) -> String{
+    match state{
+        Err(err) => "Error!".to_string(),
+        Ok(noError) => {
+            match noError{
+                Some(state) => state,
+                None => "No state!".to_string(),
+            }
+        },
+    }
+}
+
+fn main() {
     let mut runtime = Runtime::new().unwrap();
     let mut client = ImdsClient::new();
-    let lifecycle_state = runtime.block_on(get_lifecycle_state(&mut client));    
-    println!("{:?}", lifecycle_state);
+    let mut lifecycle_state = unwrap_state(runtime.block_on(get_lifecycle_state(&mut client)));
+    while lifecycle_state.ne("InService") {
+        println!("Waiting because state is {}", lifecycle_state);
+        println!("WAITING -> state is: {}", lifecycle_state);
+        lifecycle_state = unwrap_state(runtime.block_on(get_lifecycle_state(&mut client)));
+    }
+    println!("FINISHED waiting -> state is: {}", lifecycle_state);
 }
-
-//Result<Option<String>, imdsclient::Error>
