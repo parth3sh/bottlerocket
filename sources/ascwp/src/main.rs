656cb3a983f6ef7999c7d4be5661a1ee8bbd2e38
parthx <parth3sh@gmail.com>
2022-06-28 23:49:52 +0000

    Implemented basic helper program that retrieves autoscaling state using fetch_lifecycle_state and waits until state is "InService"

--- sources/ascwp/src/main.rs
+++ sources/ascwp/src/main.rs
@@ -1,35 +1,36 @@
+mod error;
 use imdsclient::ImdsClient;
+use crate::error::Result;
 use imdsclient::Error;
 use snafu::{OptionExt, ResultExt};
 use tokio::runtime::Runtime;
 use std::env;
 
+
+
 //Uses function from imds client to fetch lifecycle state
-async fn get_lifecycle_state(client: &mut ImdsClient) -> Result<Option<String>, Error> {
+async fn get_lifecycle_state(client: &mut ImdsClient) -> Result<String> {
     client.fetch_lifecycle_state().await
+    .context(error::ImdsRequestSnafu)?
+    .context(error::ImdsNoneSnafu {
+        what: "instance-id",
+    })
 }
 
-//Unwraps result and option
-fn unwrap_state(state: Result<Option<String>, Error>) -> String{
-    match state{
-        Err(err) => "Error!".to_string(),
-        Ok(noError) => {
-            match noError{
-                Some(state) => state,
-                None => "No state!".to_string(),
-            }
-        },
-    }
-}
-
-fn main() {
-    let mut runtime = Runtime::new().unwrap();
+async fn waiter() -> Result<()>{
     let mut client = ImdsClient::new();
-    let mut lifecycle_state = unwrap_state(runtime.block_on(get_lifecycle_state(&mut client)));
+    let mut lifecycle_state = get_lifecycle_state(&mut client).await?;
     while lifecycle_state.ne("InService") {
-        println!("Waiting because state is {}", lifecycle_state);
         println!("WAITING -> state is: {}", lifecycle_state);
-        lifecycle_state = unwrap_state(runtime.block_on(get_lifecycle_state(&mut client)));
+        lifecycle_state = get_lifecycle_state(&mut client).await?;
     }
-    println!("FINISHED waiting -> state is: {}", lifecycle_state);
+    println!("Finished waiting state is {}", lifecycle_state);
+    Ok(())
 }
+
+
+#[tokio::main]
+async fn main() {
+    waiter().await;
+}
+
