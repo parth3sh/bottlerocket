dc9930ddde3a23f4d3de437f2e545cffc4f958e0
parthx <parth3sh@gmail.com>
2022-07-07 14:26:28 +0000

    removed duplicate libraries

--- sources/ascwp/src/main.rs
+++ sources/ascwp/src/main.rs
@@ -15,6 +15,32 @@ async fn get_lifecycle_state(client: &mut ImdsClient) -> Result<String> {
     })
 }
 
+//Uses apiclient to get autoscaling.should-wait setting
+async fn get_autoscaling_setting<P>(socket_path: P) -> Result<model::Model>
+where
+    P: AsRef<Path>,
+{
+    let uri = "/";
+    let method = "GET";
+    trace!("{}ing from {}", method, uri);
+    let (code, response_body) = apiclient::raw_request(socket_path, &uri, method, None)
+        .await
+        .context(error::APIRequestSnafu { method, uri })?;
+
+    if !code.is_success() {
+        return error::APIResponseSnafu {
+            method,
+            uri,
+            code,
+            response_body,
+        }
+        .fail();
+    }
+    trace!("JSON response: {}", response_body);
+
+    serde_json::from_str(&response_body).context(error::ResponseJsonSnafu { method, uri })
+}
+
 async fn waiter() -> Result<()>{
     let mut client = ImdsClient::new();
     let mut lifecycle_state = get_lifecycle_state(&mut client).await?;
